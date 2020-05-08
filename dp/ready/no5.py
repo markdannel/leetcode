269780664a89801499af39007fb9efa0dceced5d
fish <wistion@foxmail.com>
2020-05-08 11:14:20 +0800

    add  dp no62

--- dp/ready/no5.py
+++ dp/ready/no5.py
@@ -12,3 +12,25 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+        dp = [[False for j in range(size)] for i in range(size)]
+        for i in range(size):
+            dp[i][i] = True
+        maxx, maxE = 1, 0
+        for i in range(size-2,-1,-1):
+            for j in range(i+1, size):
+                curL = j-i+1
+                if s[i] == s[j] and (curL <= 2 or dp[i+1][j-1]):
+                    dp[i][j] = True
+                    if curL > maxx:
+                        maxx = curL
+                        maxE = i
+                else:
+                    dp[i][j] = False
+                
+        return s[maxE:maxE+maxx]
+
+s = Solution()
+print(s.longestPalindrome("acab"))
